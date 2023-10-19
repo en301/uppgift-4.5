a9a95720bc2f3cc6e578d9369c9dfa71424bd784
en301 <emilsjostrand1000@gmail.com>
2023-10-19 20:17:48 +0200

    Add project files.

---
+++ uppgift 4.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uppgift_4_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            for(int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                siffra++;
+
+                if (siffra == 10)
+                    Console.WriteLine(0);
+                else
+                {
+                    Console.WriteLine(siffra);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
